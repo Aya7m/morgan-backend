4d9b45744167ac86dadcc3e621ee80c178d2ae1e
Aya7m <yyoyo6987@gmail.com>
2025-07-26 14:58:01 +0300

    add get product py id

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -373,3 +373,21 @@ export const getRecommendedProducts = async (req, res) => {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 };
+
+
+// get product by id
+export const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({
+            message: "Product fetched successfully",
+            data: product
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
