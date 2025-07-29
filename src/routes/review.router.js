2ab9503578bdc0e5a46be8e9a8a56a3b58c14bb3
Aya7m <yyoyo6987@gmail.com>
2025-07-29 12:52:17 +0300

    addReviewRouter

--- src/routes/review.router.js
+++ src/routes/review.router.js
@@ -4,6 +4,6 @@ const reviewRouter = Router();
 import { auth } from "../middleware/auth.middleware.js";
 import { addReview, getProductReviews } from "../controllers/review.controller.js";
 reviewRouter.post('/create', auth(), addReview)
-reviewRouter.get('/get-all/:productId',auth(),getProductReviews) // Assuming you have a function to get all reviews
+reviewRouter.get('/get-all/:productId',getProductReviews) // Assuming you have a function to get all reviews
 
 export default reviewRouter;
