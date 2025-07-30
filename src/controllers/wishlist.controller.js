680694f5a4e656d032f47d3a3264ea86853669b5
Aya7m <yyoyo6987@gmail.com>
2025-07-30 13:18:54 +0300

    addTowishlist

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -2,21 +2,32 @@ import { Wishlist } from "../models/wishlist.model.js";
 
 // controllers/wishlist.controller.js
 export const addToWishlist = async (req, res) => {
-    const userId = req.authuser._id;
-    const { productId } = req.body;
+    try {
+        const userId = req.authuser._id;
+        const { productId } = req.body;
 
-    let wishlist = await Wishlist.findOne({ userId });
-    if (!wishlist) {
-        wishlist = await Wishlist.create({ userId, products: [productId] });
-    } else {
-        if (!wishlist.products.includes(productId)) {
-            wishlist.products.push(productId);
-            await wishlist.save();
+        if (!productId) {
+            return res.status(400).json({ message: "Product ID is required" });
         }
-    }
 
-    res.status(200).json({ message: "Added to wishlist", wishlist });
+        let wishlist = await Wishlist.findOne({ userId });
+
+        if (!wishlist) {
+            wishlist = await Wishlist.create({ userId, products: [productId] });
+        } else {
+            if (!wishlist.products.includes(productId)) {
+                wishlist.products.push(productId);
+                await wishlist.save();
+            }
+        }
+
+        res.status(200).json({ message: "Added to wishlist", wishlist });
+    } catch (error) {
+        console.error("Add to Wishlist Error:", error);
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
 };
+
 
 
 export const removeFromWishlist = async (req, res) => {
