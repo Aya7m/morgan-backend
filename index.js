22ef19d2f4efb2c65351145416851f4187f8141e
Aya7m <yyoyo6987@gmail.com>
2025-08-07 13:01:05 +0300

    add port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express'
 const app = express()
-const port = 3000
+
 import dotenv from 'dotenv'
+const port = process.env.PORT||5000
 import cors from 'cors'
 import db_connection from './src/db/connection.js'
 
@@ -21,8 +22,9 @@ dotenv.config()
 
 db_connection();
 app.use(cors({
-  origin: 'http://localhost:3000', // أو ['http://localhost:3000'] لو أكتر من origin
-  credentials: true, // لو بتستخدم كوكيز أو توكنات
+  origin: '*', // Allow all origins, you can specify specific origins if needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
 }));
 
 app.use(express.json());
